439e2050f946423c69bf89c8bca869d2f9a1484f
Margarita-P <79039786+Margarita-P@users.noreply.github.com>
2023-12-17 15:51:48 +0200

    Update Option1.py

--- Option1.py
+++ Option1.py
@@ -1,7 +1,10 @@
 import numpy as np
 from scipy.io import wavfile
 
-def stereo_to_mono(input_file, output_file):
+def is_channel_empty(channel_data):
+    return np.all(channel_data == 0)  # Check if all values in the channel are zeros
+
+def stereo_to_mono_non_empty(input_file, output_file):
     # Read the stereo audio file
     sample_rate, stereo_data = wavfile.read(input_file)
 
@@ -10,8 +13,23 @@ def stereo_to_mono(input_file, output_file):
         print("Input file is not stereo.")
         return
 
-    # Convert stereo to mono by averaging left and right channels
-    mono_data = np.mean(stereo_data, axis=1, dtype=np.int16)
+    left_channel = stereo_data[:, 0]
+    right_channel = stereo_data[:, 1]
+
+    # Check if either channel is empty (contains only zeros)
+    left_empty = is_channel_empty(left_channel)
+    right_empty = is_channel_empty(right_channel)
+
+    if left_empty and right_empty:
+        print("Both channels are empty.")
+        return
+    elif left_empty:
+        mono_data = right_channel  # Use right channel if left channel is empty
+    elif right_empty:
+        mono_data = left_channel   # Use left channel if right channel is empty
+    else:
+        # If both channels have audio, take the average
+        mono_data = np.mean(stereo_data, axis=1, dtype=np.int16)
 
     # Write the mono audio to a WAV file
     wavfile.write(output_file, sample_rate, mono_data)
@@ -20,5 +38,6 @@ def stereo_to_mono(input_file, output_file):
 input_audio = 'C:/Users/scerb/Desktop/uni/Semestras 7/Garso apdorojimas/Python/Stereo_music.wav'  # Replace with your stereo audio file
 output_audio = 'C:/Users/scerb/Desktop/uni/Semestras 7/Garso apdorojimas/Python/output_mono_audio.wav'  # Name for the output mono audio file
 
-# Convert stereo to mono
-stereo_to_mono(input_audio, output_audio)
+# Convert stereo to mono considering non-empty channel
+stereo_to_mono_non_empty(input_audio, output_audio)
+
